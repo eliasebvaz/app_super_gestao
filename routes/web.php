9c46d4be770d82564c905b07906051b13aa9de55
Elias Batista Vaz Junior <eliasebvaz@gmail.com>
2022-09-19 08:40:55 -0300

    Middleware config geral

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\LogAcessoMiddleware;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +15,7 @@ use App\Http\Middleware\LogAcessoMiddleware;
 
 // Rota principal
 Route::get('/', [\App\Http\Controllers\PrincipalController::class,'principal'])
-    ->name('site.index')
-    ->middleware(LogAcessoMiddleware::class);
+    ->name('site.index');
 
 Route::get('/sobrenos', [\App\Http\Controllers\SobreNosController::class,'principal'])->name('site.sobrenos');
 Route::get('/contato', [\App\Http\Controllers\ContatoController::class,'contato'])->name('site.contato');
