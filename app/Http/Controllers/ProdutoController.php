563e4f73120c61f630f16dccf3e398e20a83247e
Elias Batista Vaz Junior <eliasebvaz@gmail.com>
2022-09-22 16:36:51 -0300

    Confirmando conhecimento - 5

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Produto;
+use App\Models\Unidade;
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
@@ -12,10 +13,11 @@ class ProdutoController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-        echo 'index';
+        $produtos = Produto::paginate(10);
+
+        return view ('app.produto.index', ['produtos'=> $produtos, 'request' => $request->all()]);
     }
 
     /**
@@ -25,8 +27,8 @@ class ProdutoController extends Controller
      */
     public function create()
     {
-        //
-        echo 'create';
+        $unidades = Unidade::all();
+        return view ('app.produto.create', ['unidades'=> $unidades]);
     }
 
     /**
@@ -37,8 +39,30 @@ class ProdutoController extends Controller
      */
     public function store(Request $request)
     {
-        //
-        echo 'store';
+
+        // Cadastro do registro
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:2000',
+            'peso' => 'required|integer',
+            // Para verificação na tabela, coluna
+            'unidade_id' => 'exists:unidades,id'
+        ];
+
+        $feedback = [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'descricao.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'descricao.max' => 'O campo nome deve ter no máximo 2000 caracteres',
+            'peso.integer' => 'O campo peso deve ser um número inteiro',
+            'unidade_id.exists' => 'O unidade de medida informada não existe',
+        ];
+
+        $request->validate($regras, $feedback);
+
+        Produto::create($request->all());
+        return redirect()->route('produto.index');
     }
 
     /**
