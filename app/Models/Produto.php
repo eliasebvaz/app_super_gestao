03d1d4b674a17f20c70a838ce147dfd0cf6a81f0
Elias Batista Vaz Junior <eliasebvaz@gmail.com>
2022-09-26 06:09:37 -0300

    Confirmando conhecimento - 8

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -9,4 +9,18 @@ class Produto extends Model
 {
     use HasFactory;
     protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id'];
+
+    /*
+        |--------------------------------------------------------------------------
+        | Função de relacionamento entre as Tabelas de produtos e produto_detalhes
+        |--------------------------------------------------------------------------
+        |
+        | Nesta função, ela irá procurar com base no id da tabela produtos
+        | a foreign key na tabela produto_detalhes, adicionando as informações
+        | que forem relacionadas com base de relacionamento 1x1
+        |
+    */
+    public function ProdutoDetalhe(){
+        return $this->hasOne('App\Models\ProdutoDetalhe');
+    }
 }
